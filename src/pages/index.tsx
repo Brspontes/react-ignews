93cf0210eb4cdc77801e19f4d75f9ac654e51f2a
Brspontes <brian.robert16@hotmail.com>
2021-04-06 08:34:37 -0300

    criando estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>Home</Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>World</span>
       </h1>
     </>
