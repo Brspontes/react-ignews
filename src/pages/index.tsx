2242c855adb602127a1d6a4ffdfc7ab3521b17cc
Brspontes <brian.robert16@hotmail.com>
2021-04-13 08:32:54 -0300

    utilizando server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>Home</Head>
@@ -11,11 +20,28 @@ export default function Home() {
           <span>👏 Hey welcome</span>
           <h1>News about <br /> the <span>React</span> world.</h1>
           <p>Get access to all publications <br />
-          <span>for $9.90 month</span></p>
-          <SubscribeButton />
+          <span>for { product.amount } month</span></p>
+          <SubscribeButton priceId={ product.priceId } />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ifk06I7n9oMViJCVEhGxwRd', { expand: ['product'] })
+  const product =  {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format((price.unit_amount / 100))
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
