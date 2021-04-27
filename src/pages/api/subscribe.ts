9a8e2e7d688fde066448ec0447a68fe84d8f5cf4
Brspontes <brian.robert16@hotmail.com>
2021-04-27 20:22:45 -0300

    desenvolvendo metodo de pagamento

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,30 +1,65 @@
+import { fauna } from './../../services/fauna';
+import { query } from 'faunadb'
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 import { stripe } from "../../services/stripe";
 
+type User = {
+  ref: {
+    id: string
+  },
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req })
 
+    const user = await fauna.query<User>(
+      query.Get(
+        query.Match(
+          query.Index('user_email'),
+          query.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id
+    if(!customerId) {
       const stripeCustomer = await stripe.customers.create({
         email: session.user.email
       })
-  
-      const stripeCheckoutSession = await stripe.checkout.sessions.create({
-        customer: stripeCustomer.id,
-        payment_method_types: ['card'],
-        billing_address_collection: 'required',
-        line_items: [
-          { price:'price_1Ifk06I7n9oMViJCVEhGxwRd', quantity: 1 }
-        ],
-        mode: 'subscription',
-        allow_promotion_codes: true,
-        success_url: process.env.STRIPE_SUCCESS_URL,
-        cancel_url: process.env.STRIPE_CANCEL_URL
-      })
-  
-      return res.status(200).json({ sessionId: stripeCheckoutSession.id })  
+
+      await fauna.query(
+        query.Update(
+          query.Ref(query.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price:'price_1Ifk06I7n9oMViJCVEhGxwRd', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })  
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
